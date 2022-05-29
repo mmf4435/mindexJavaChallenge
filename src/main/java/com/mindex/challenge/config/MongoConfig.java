80cc44827177028445aafe6de2a79184a49c8926
Matt Favazza <mmf4435@rit.edu>
2022-05-29 18:21:18 -0400

    test

--- src/main/java/com/mindex/challenge/config/MongoConfig.java
+++ src/main/java/com/mindex/challenge/config/MongoConfig.java
@@ -1,3 +1,6 @@
+
+
+
 package com.mindex.challenge.config;
 
 import com.mindex.challenge.dao.EmployeeRepository;
@@ -7,6 +10,7 @@ import de.bwaldvogel.mongo.MongoServer;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
@@ -15,14 +19,30 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackageClasses = EmployeeRepository.class)
 @Configuration
 public class MongoConfig{
+    //template for employee repo
+    @Primary
     @Bean
     public MongoTemplate mongoTemplate(MongoClient mongoClient) {
         return new MongoTemplate(mongoDbFactory(mongoClient));
     }
 
+    //template for compensation repo
+    @Bean(name = "compTemplate")
+    public MongoTemplate compTemplate(MongoClient client){
+        return new MongoTemplate(compMongoDbFactory(client));
+    }
+
+    //factory for employee repo
+    @Primary
     @Bean
     public MongoDbFactory mongoDbFactory(MongoClient mongoClient) {
         return new SimpleMongoClientDbFactory(mongoClient, "test");
+    }
+
+    //factory for compensation repo
+    @Bean
+    public MongoDbFactory compMongoDbFactory(MongoClient client){
+        return new SimpleMongoClientDbFactory(client, "test2");
     }
 
     @Bean(destroyMethod="shutdown")
@@ -37,5 +57,3 @@ public class MongoConfig{
         return MongoClients.create("mongodb:/" + mongoServer().getLocalAddress());
     }
 }
-
-
